d184a35e174035f644c63d5c28bbfaf56b1ea045
Hugo <83752970+hugo6866@users.noreply.github.com>
2022-12-25 11:56:42 +0700

    refactoring and using GSON instead

--- app/src/main/java/com/ppb13937/aplikasikalkulator/MainActivity.java
+++ app/src/main/java/com/ppb13937/aplikasikalkulator/MainActivity.java
@@ -7,25 +7,21 @@ import androidx.recyclerview.widget.RecyclerView;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
-import android.widget.Switch;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonArray;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
 
+import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Objects;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     EditText editText1, editText2;
@@ -119,128 +115,79 @@ public class MainActivity extends AppCompatActivity {
     public boolean isHistoryExist(int num1,int num2,String operator, int result){
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         String jsonHistory = sharedPreferences.getString("pref_data","");
-        try {
-            JSONObject jsonObj = new JSONObject(jsonHistory);
-            JSONArray jsonArray = jsonObj.getJSONArray("history");
-            if(jsonArray.length() == 0) return false;
-            for (int i = 0; i < jsonArray.length(); i++) {
-                JSONObject obj = jsonArray.getJSONObject(i);
-                if(obj.getInt("num1") == num1 && obj.getInt("num2") == num2 && obj.getString("operator").equals(operator) && obj.getInt("result") == result) return true;
+        Gson gson = new Gson();
+        History[] historyArray = gson.fromJson(jsonHistory, History[].class);
+        if(historyArray != null) {
+            for (History history : historyArray) {
+                if (Integer.parseInt(history.getNum1()) == num1 && Integer.parseInt(history.getNum2()) == num2 && history.getOperator().equals(operator) && Integer.parseInt(history.getResult()) == result)
+                    return true;
             }
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
         return false;
     }
 
 
+
     public void saveHistory(int num1, int num2, String operator, int result) {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        try {
-            JSONObject jo = new JSONObject();
-            jo.put("num1", num1);
-            jo.put("num2", num2);
-            jo.put("operator", operator);
-            jo.put("result", result);
-            JSONArray ja = new JSONArray();
-            listHistory.add(new History(num1, num2, operator, result));
-            ja.put(jo);
-            JSONObject mainObj = new JSONObject();
-            mainObj.put("history", ja);
-            editor.putBoolean("initialized", true);
-            editor.putString("pref_data", mainObj.toString()).commit();
-        } catch (JSONException json) {
-        }
+        Gson gson = new GsonBuilder().create();
+        listHistory.add(new History(num1, num2, operator, result));
+        String json = gson.toJson(listHistory);
+
+        editor.putBoolean("initialized", true);
+        editor.putString("history", json);
         editor.apply();
     }
 
-    public void updateHistory(int num1, int num2, String operator, int result) {
-        SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        public void updateHistory(int num1, int num2, String operator, int result) {
+            SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
 
-        History history = new History(num1, num2, operator, result);
-        listHistory.add(history);
+            History history = new History(num1, num2, operator, result);
+            listHistory.add(history);
 
-        try {
-            JSONArray historyArray = new JSONArray();
-            for (History h : listHistory) {
-                JSONObject historyObject = new JSONObject();
-                historyObject.put("num1", h.getNum1());
-                historyObject.put("num2", h.getNum2());
-                historyObject.put("operator", h.getOperator());
-                historyObject.put("result", h.getResult());
-                historyArray.put(historyObject);
-            }
+            Gson gson = new Gson();
 
-            JSONObject mainObject = new JSONObject();
-            mainObject.put("history", historyArray);
+            String json = gson.toJson(listHistory);
 
             editor.putBoolean("initialized", true);
-            editor.putString("pref_data", mainObject.toString()).commit();
+            editor.putString("pref_data", json).commit();
             editor.apply();
-        } catch (JSONException e) {
-            e.printStackTrace();
         }
-    }
+
 
     public void loadHistory() {
         SharedPreferences sharedPreferences = getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         String jsonHistory = sharedPreferences.getString("pref_data", "");
         listHistory.clear();
 
-        try {
-            JSONObject jsonObj = new JSONObject(jsonHistory);
-            JSONArray mJsonArrayProperty = jsonObj.getJSONArray("history");
-            if (mJsonArrayProperty.length() == 0) return;
+        Gson gson = new Gson();
+        Type type = new TypeToken<List<History>>() {}.getType();
+        List<History> historyList = gson.fromJson(jsonHistory, type);
 
-            for (int i = 0; i < mJsonArrayProperty.length(); i++) {
-                JSONObject history = mJsonArrayProperty.getJSONObject(i);
-                listHistory.add(new History(history.getInt("num1"), history.getInt("num2"), history.getString("operator"), history.getInt("result")));
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
+        if (historyList == null || historyList.isEmpty()) {
+            return;
         }
+
+        listHistory.addAll(historyList);
 
         recHistory.setAdapter(new HistoryAdapter(listHistory, this));
         recHistory.setLayoutManager(new LinearLayoutManager(this));
     }
 
+
     public static void reloadData(Context context) {
         SharedPreferences sharedPrefs = context.getSharedPreferences(SHARED_PREFS, MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPrefs.edit();
-        String jsonHistory = sharedPrefs.getString("pref_data", "");
-        try {
-            JSONObject jsonObj = new JSONObject(jsonHistory);
-            JSONArray mJsonArrayProperty = jsonObj.getJSONArray("history");
-            try {
-                if (listHistory.size() == 0) {
-                    sharedPrefs.edit().clear().commit();
-                    recHistory.setAdapter(new HistoryAdapter(listHistory, context));
-                    recHistory.setLayoutManager(new LinearLayoutManager(recHistory.getContext()));
-                    return;
-                }
-                JSONArray ja = new JSONArray();
-                for (int i = 0; i < listHistory.size(); i++) {
-                    History history = listHistory.get(i);
-                    JSONObject historyObj = new JSONObject();
-                    historyObj.put("num1", history.getNum1());
-                    historyObj.put("num2", history.getNum2());
-                    historyObj.put("operator", history.getOperator());
-                    historyObj.put("result", history.getResult());
-                    ja.put(historyObj);
-                }
-                JSONObject mainObj = new JSONObject();
-                mainObj.put("history", ja);
-                editor.putBoolean("initialized", true);
-                editor.putString("pref_data", mainObj.toString()).commit();
-                editor.apply();
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
+
+        Gson gson = new Gson();
+        String jsonHistory = gson.toJson(listHistory);
+
+        editor.putBoolean("initialized", true);
+        editor.putString("pref_data", jsonHistory).commit();
+        editor.apply();
+
         recHistory.setAdapter(new HistoryAdapter(listHistory, context));
         recHistory.setLayoutManager(new LinearLayoutManager(recHistory.getContext()));
     }
